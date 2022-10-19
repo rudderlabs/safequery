c2ad84d36a8d315f93818e67c3fbd3d7246e4cf0
Leonidas Vrachnis <leo.al.vra@gmail.com>
2022-10-19 22:39:55 +0300

    feat: proof of concept

---
+++ safequery.go
@@ -0,0 +1,54 @@
+package safequery
+
+import (
+	"regexp"
+	"strconv"
+
+	"github.com/lib/pq"
+)
+
+type Query struct {
+	query string
+	args  []any
+}
+
+var matchDollar = regexp.MustCompile(`\s\$\$?\d+`)
+
+func (q *Query) Add(text string, args []any) {
+	shift := len(q.args)
+
+	q.query += matchDollar.ReplaceAllStringFunc(text, func(match string) string {
+		match = match[1:]
+
+		if match[1] == '$' {
+			// double dollar mode
+
+			index, err := strconv.Atoi(match[2:])
+			if err != nil {
+				panic(err)
+			}
+
+			identifier := pq.QuoteIdentifier(args[index-1].(string))
+			shift--
+			args = append(args[:index-1], args[index:]...)
+
+			return " " + identifier
+		}
+
+		index, err := strconv.Atoi(match[1:])
+		if err != nil {
+			panic(err)
+		}
+
+		return " $" + strconv.Itoa(shift+index)
+	})
+	q.args = append(q.args, args...)
+}
+
+func (q *Query) Query() string {
+	return q.query
+}
+
+func (q *Query) Args() []any {
+	return q.args
+}
